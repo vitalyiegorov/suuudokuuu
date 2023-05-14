5cd3626bbf3865b0cef143574385d575861f8f85
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-05-14 14:45:36 +0200

    feat: added game field with blank spaces

--- store/app-root/app-root.selectors.ts
+++ store/app-root/app-root.selectors.ts
@@ -3,5 +3,5 @@ import { createSelector } from '@reduxjs/toolkit';
 import { type RootState } from '../create-store';
 
 const appRootSelector = (state: RootState) => state.appRoot;
-export const appRootFieldSelector = createSelector(appRootSelector, state => state.field);
+export const appRootFieldSelector = createSelector(appRootSelector, state => state.gameField);
 export const appRootSelectedCellSelector = createSelector(appRootSelector, state => state.selectedCell);
---
+++ utils/create-game-field.util.ts
@@ -0,0 +1,15 @@
+import { BlankCellValue } from '../interfaces/blank-cell-value';
+import { type CellInterface } from '../interfaces/cell.interface';
+
+const cloneField = (field: CellInterface[][]): CellInterface[][] => field.map(row => row.map(cell => ({ ...cell })));
+const getRandomPosition = (field: CellInterface[][]): number => Math.floor(Math.random() * field.length);
+
+export const createGameField = (originalField: CellInterface[][], blankCellsCount: number): CellInterface[][] => {
+    const newField = cloneField(originalField);
+
+    for (let i = 0; i < blankCellsCount; i++) {
+        newField[getRandomPosition(originalField)][getRandomPosition(originalField)].value = BlankCellValue;
+    }
+
+    return newField;
+};
