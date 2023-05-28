fbe99a4167e09f20cba6515a62dc2188540bbc2d
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-05-28 22:25:54 +0200

    ci: added xcode cloud script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# https://developer.apple.com/documentation/xcode/writing-custom-build-scripts
+
+# Set the -e flag to stop running the script in case a command returns
+# a nonzero exit code.
+set -e
+
 # Install CocoaPods and yarn using Homebrew.
 brew install cocoapods
 brew install node@18
@@ -9,3 +15,7 @@ brew install yarn
 # Install dependencies
 yarn
 pod install
+
+# A command or script succeeded.
+echo "A command or script was successful."
+exit 0
