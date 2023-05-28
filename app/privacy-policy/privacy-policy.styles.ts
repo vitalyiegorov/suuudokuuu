39aa28fee8766109dcb9686f1821ad07545354b8
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-05-28 09:01:48 +0200

    fix: fixed privacy policy return button

--- app/privacy-policy/privacy-policy.styles.ts
+++ app/privacy-policy/privacy-policy.styles.ts
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 
-import { Colors } from '../../src/@generic/styles/theme';
+import { Colors } from '../../src/@generic';
 
 export const PrivacyPolicyStyles = StyleSheet.create({
     container: {
