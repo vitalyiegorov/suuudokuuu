f2c303d85fd41f9019c0f5368b1da5f70d355044
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-05-29 10:00:29 +0200

    feat: started creation of game logic class with string representation

--- src/game/utils/create-cell.util.ts
+++ src/game/utils/create-cell.util.ts
@@ -1,7 +1,8 @@
 import { BlankCellValueConstant } from '../constants/blank-cell-value.constant';
 import { FieldGroupHeightConstant, FieldGroupWidthConstant } from '../constants/field.constant';
 
-const getGroupValue = (x: number, y: number): number =>
+// TODO: Flip x/y args
+export const getGroupValue = (x: number, y: number): number =>
     Math.floor(x / FieldGroupWidthConstant) * FieldGroupWidthConstant + Math.floor(y / FieldGroupHeightConstant) + 1;
 
 export const createCell = (x: number, y: number, value = BlankCellValueConstant) => ({ group: getGroupValue(x, y), value, x, y });
