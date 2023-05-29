7c5e26e466e87ce2ad57572ec578747146c93923
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-05-29 15:17:01 +0200

    refactor: trying to merge game logic in OOP class

--- src/game/utils/field/has-value-in-row.util.ts
+++ src/game/utils/field/has-value-in-row.util.ts
@@ -2,8 +2,8 @@ import { type CellInterface } from '../../interfaces/cell.interface';
 import { type FieldInterface } from '../../interfaces/field.interface';
 
 export const hasValueInRow = (cell: CellInterface, field: FieldInterface): boolean => {
-    for (let col = 0; col < field.length; col++) {
-        if (field[cell.y][col].value === cell.value) {
+    for (let x = 0; x < field.length; x++) {
+        if (field[cell.y][x].value === cell.value) {
             return true;
         }
     }
