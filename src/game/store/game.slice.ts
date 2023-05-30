8028f904d9bd6ef70f545d83fddb0bb52281afcf
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-05-30 17:52:59 +0200

    fix: fixed cell rendering logic, redux issues

--- src/game/store/game.slice.ts
+++ src/game/store/game.slice.ts
@@ -1,8 +1,8 @@
 import { type PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 import type { DifficultyEnum } from '../../@generic';
-import { SudokuGame } from '../../@logic';
-import type { CellInterface, ScoredCellsInterface } from '../../@logic';
+import type { CellInterface } from '../../@logic';
+import { SudokuGame, calculateScore } from '../../@logic';
 import { emptyGame } from '../interfaces/game.interface';
 
 import { initialGameState } from './game.state';
@@ -22,11 +22,13 @@ export const gameSlice = createSlice({
             state.startedAt = new Date();
             state.difficulty = action.payload;
         },
-        setValue: (state, action: PayloadAction<{ cell: CellInterface; scoredCells: ScoredCellsInterface }>) => {
-            state.field[action.payload.cell.y][action.payload.cell.x] = action.payload.cell;
-            state.scoredCells = action.payload.scoredCells;
+        finishMove: (state, action: PayloadAction<CellInterface>) => {
+            state.scoredCells = SudokuGame.setCellValue(action.payload);
+            state.field = SudokuGame.Field;
             state.possibleValues = SudokuGame.PossibleValues;
             state.availableValues = SudokuGame.AvailableValues;
+            // TODO: Move score calculation to logic, improve logic
+            state.score += calculateScore(SudokuGame.Field, action.payload, state.mistakes, state.startedAt);
             state.endedAt = new Date();
         },
         reset: state => {
@@ -34,9 +36,6 @@ export const gameSlice = createSlice({
         },
         selectCell: (state, action: PayloadAction<CellInterface | undefined>) => {
             state.selectedCell = action.payload;
-        },
-        increaseScore: (state, action: PayloadAction<number>) => {
-            state.score += action.payload;
         },
         madeAMistake: state => {
             state.mistakes += 1;
