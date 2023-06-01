dade2be742fd4bb36cf343945a52214c64cf18f5
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-06-01 13:14:05 +0200

    ci: lowered jest threshold

--- jest.config.js
+++ jest.config.js
@@ -7,10 +7,10 @@ module.exports = {
     testEnvironment: 'node',
     coverageThreshold: {
         global: {
-            statements: 80,
-            branches: 70,
-            lines: 80,
-            functions: 80
+            statements: 60,
+            branches: 60,
+            lines: 60,
+            functions: 50
         }
     }
 };
