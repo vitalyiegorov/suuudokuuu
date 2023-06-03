63a2373489c318e38fa880e8360d113df5fc83c2
Vitalii Yehorov <vitalyiegorov@gmail.com>
2023-06-03 16:26:51 +0200

    chore: fixed deaadcode

--- .ts-prunerc.js
+++ .ts-prunerc.js
@@ -1,4 +1,4 @@
 module.exports = {
-    ignore: '(dist|build|public|report)/|index.ts|_layout.tsx|.web.tsx?',
+    ignore: '(dist|build|public|report)/|index.ts|_layout.tsx|.web.tsx?|selectors.ts',
     error: true
 };
