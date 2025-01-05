284f19924e1471b7ce794fcd1a57e2c5d9094433
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-01-05 16:49:07 +0100

    feat: expo 52 update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
-import { SplashScreen, Stack } from 'expo-router';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
@@ -6,21 +8,30 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { Inter_500Medium, Inter_700Bold, useFonts } from '@expo-google-fonts/inter';
 import { ThemeProvider } from '@react-navigation/native';
+import { isDefined } from '@rnw-community/shared';
 
 import { appRootPersistor, appRootStore } from '../src/@app-root';
 import { BlackTheme, WhiteTheme } from '../src/@generic';
+
+void SplashScreen.preventAutoHideAsync();
 
 const stackOptions = { headerShown: false, gestureEnabled: false };
 
 export default function RootLayout() {
     const scheme = useColorScheme() === 'dark' ? BlackTheme : WhiteTheme;
-    const [fontsLoaded] = useFonts({
+    const [loaded, error] = useFonts({
         Inter_500Medium,
         Inter_700Bold
     });
 
-    if (!fontsLoaded) {
-        return <SplashScreen />;
+    useEffect(() => {
+        if (loaded || isDefined(error)) {
+            void SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !isDefined(error)) {
+        return null;
     }
 
     return (
