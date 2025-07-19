5865e3da08511caad447121f8266f7af96a052c3
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-07-19 15:41:22 +0200

    feat: web theme color status bars

--- src/@generic/components/page-header/page-header.tsx
+++ src/@generic/components/page-header/page-header.tsx
@@ -1,16 +1,23 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'react-native';
 
 import { isNotEmptyString } from '@rnw-community/shared';
 
 import { PageTitleConstant } from '../../constants/text.constants';
+import { useHtmlThemeColor } from '../../hooks/use-html-theme-color.hook';
 
 interface Props {
     readonly title?: string;
 }
 
 export const PageHeader = ({ title = '' }: Props) => {
+    const theme = useColorScheme();
+
     const fullTitle = isNotEmptyString(title) ? `${PageTitleConstant} - ${title}` : PageTitleConstant;
+    const baseColor = theme === 'dark' ? '#010101' : '#f2f2f2';
+
+    useHtmlThemeColor(baseColor);
 
     return (
         <>
