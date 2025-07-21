aae03696a829ffdff5d9bd9cd32c991b93995a0e
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-07-21 10:16:25 +0200

    chore: format

--- packages/app/src/game/components/available-values-item/available-values-item.tsx
+++ packages/app/src/game/components/available-values-item/available-values-item.tsx
@@ -33,46 +33,47 @@ export interface AvailableValuesItemRef {
     triggerAnimation: () => void;
 }
 
-// TODO: Add animation when correct value is selected
-export const AvailableValuesItem = forwardRef<AvailableValuesItemRef, Props>(({ value, isActive, onSelect, progress, correctValue, canPress }, ref) => {
-    const isCorrect = value === correctValue;
-    const pressAnimatedBgColor = isCorrect ? Colors.cell.active : Colors.cell.error;
+export const AvailableValuesItem = forwardRef<AvailableValuesItemRef, Props>(
+    ({ value, isActive, onSelect, progress, correctValue, canPress }, ref) => {
+        const isCorrect = value === correctValue;
+        const pressAnimatedBgColor = isCorrect ? Colors.cell.active : Colors.cell.error;
 
-    const animated = useSharedValue(0);
-    const animatedStyles = useAnimatedStyle(() => ({
-        backgroundColor: interpolateColor(animated.value, [0, 1], [Colors.white, pressAnimatedBgColor]),
-        ...(!isCorrect && {
-            transform: [
-                { translateX: interpolate(animated.value, [0, 0.5, 1], [0, -10, 10]) },
-                { rotate: `${interpolate(animated.value, [0, 0.5, 1], [0, -20, 20])}deg` }
-            ]
-        })
-    }));
+        const animated = useSharedValue(0);
+        const animatedStyles = useAnimatedStyle(() => ({
+            backgroundColor: interpolateColor(animated.value, [0, 1], [Colors.white, pressAnimatedBgColor]),
+            ...(!isCorrect && {
+                transform: [
+                    { translateX: interpolate(animated.value, [0, 0.5, 1], [0, -10, 10]) },
+                    { rotate: `${interpolate(animated.value, [0, 0.5, 1], [0, -20, 20])}deg` }
+                ]
+            })
+        }));
 
-    const triggerAnimationFn = () => {
-        animated.value = withSequence(withTiming(1, { duration: 200 }), withTiming(0, { duration: 200 }));
-    };
+        const triggerAnimationFn = () => {
+            animated.value = withSequence(withTiming(1, { duration: 200 }), withTiming(0, { duration: 200 }));
+        };
 
-    useImperativeHandle(ref, () => ({
-        triggerAnimation: triggerAnimationFn
-    }));
+        useImperativeHandle(ref, () => ({
+            triggerAnimation: triggerAnimationFn
+        }));
 
-    const handlePress = () => {
-        triggerAnimationFn();
-        onSelect(value);
-    };
+        const handlePress = () => {
+            triggerAnimationFn();
+            onSelect(value);
+        };
 
-    const buttonStyles = [styles.button, cs(isActive, styles.wrapperActive), animatedStyles];
-    const textStyles = [styles.text, cs(isActive, styles.textActive)];
-    const progressStyles = [styles.progress, { width: `${progress}%` }] as StyleProp<ViewStyle>;
+        const buttonStyles = [styles.button, cs(isActive, styles.wrapperActive), animatedStyles];
+        const textStyles = [styles.text, cs(isActive, styles.textActive)];
+        const progressStyles = [styles.progress, { width: `${progress}%` }] as StyleProp<ViewStyle>;
 
-    return (
-        <View style={styles.container} testID={selectors.Root}>
-            <ReanimatedPressable key={value} style={buttonStyles} testID={selectors.Button} {...(canPress && { onPress: handlePress })}>
-                <Text style={textStyles}>{value}</Text>
-            </ReanimatedPressable>
+        return (
+            <View style={styles.container} testID={selectors.Root}>
+                <ReanimatedPressable key={value} style={buttonStyles} testID={selectors.Button} {...(canPress && { onPress: handlePress })}>
+                    <Text style={textStyles}>{value}</Text>
+                </ReanimatedPressable>
 
-            <View style={progressStyles} />
-        </View>
-    );
-});
+                <View style={progressStyles} />
+            </View>
+        );
+    }
+);
