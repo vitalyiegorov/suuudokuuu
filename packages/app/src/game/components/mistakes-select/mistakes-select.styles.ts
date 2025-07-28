dcf853c09c542d6755744c63481c526554a0145b
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-07-28 16:14:48 +0200

    feat(app): added max mistakes selector, hardcore mode

--- packages/app/src/game/components/mistakes-select/mistakes-select.styles.ts
+++ packages/app/src/game/components/mistakes-select/mistakes-select.styles.ts
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export const MistakesSelectStyles = StyleSheet.create({
+    hardcoreText: {
+        fontWeight: 'bold'
+    },
     wrapper: {
         marginBottom: 20,
         rowGap: 5
--- packages/app/src/game/components/mistakes-select/mistakes-select.tsx
+++ packages/app/src/game/components/mistakes-select/mistakes-select.tsx
@@ -1,8 +1,10 @@
 import { useLingui } from '@lingui/react/macro';
+import { use } from 'react';
 import { View } from 'react-native';
 
 import { BlackButton } from '../../../@generic/components/black-button/black-button';
 import { Header } from '../../../@generic/components/header/header';
+import { ThemeContext } from '../../../@generic/context/theme.context';
 
 import { MistakesSelectStyles as styles } from './mistakes-select.styles';
 
@@ -13,17 +15,20 @@ interface Props {
 }
 
 export const MistakesSelect = ({ onSelect }: Props) => {
+    const { theme } = use(ThemeContext);
     const { t } = useLingui();
 
     const handlePress = (maxMistakes: number) => () => void onSelect(maxMistakes);
+
+    const hardcoreStyle = [styles.hardcoreText, { color: theme.colors.red }];
 
     return (
         <View style={styles.wrapper}>
             <Header text={t`Choose your challenge`} />
 
-            <BlackButton onPress={handlePress(99)} text={t`Immortal - 99`} />
-            <BlackButton onPress={handlePress(3)} text={t`Standard - 3`} />
-            <BlackButton onPress={handlePress(0)} text={t`Hardcore - 0`} />
+            <BlackButton onPress={handlePress(99)} text={t`Immortal - 99 mistakes`} />
+            <BlackButton onPress={handlePress(3)} text={t`Standard - 3 mistakes`} />
+            <BlackButton onPress={handlePress(0)} styleText={hardcoreStyle} text={t`Hardcore`} />
         </View>
     );
 };
