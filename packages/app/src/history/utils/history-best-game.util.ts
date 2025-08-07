2d3657fc0f9470e66ff844fe87c2940071b28b3e
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-08-07 15:28:42 +0200

    fix(app): browser navigation back

--- packages/app/src/history/utils/history-best-game.util.ts
+++
@@ -1,7 +0,0 @@
-import type { GameState } from '../../game/store/game.state';
-
-export const historyBestGame = (history: GameState): [number, number] =>
-    Object.values(history.historyByDifficulty).reduce(
-        (max, gameHistory) => (gameHistory.bestScore > max[0] ? [gameHistory.bestScore, gameHistory.bestTime] : max),
-        [0, 0]
-    );
