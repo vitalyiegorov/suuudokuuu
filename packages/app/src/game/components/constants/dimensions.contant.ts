09ec784f99c002e730173bc9d04624e018c18304
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-08-07 07:51:45 +0200

    feat(app): make phone cell size dynamic to support more screens

--- packages/app/src/game/components/constants/dimensions.contant.ts
+++ packages/app/src/game/components/constants/dimensions.contant.ts
@@ -3,24 +3,15 @@ import { Dimensions } from 'react-native';
 
 const { height, width } = Dimensions.get('window');
 
-const getCellSize = (): number => {
+const getCellSize = (screenMarginPercentage = 4): number => {
     // TODO: We need orientation to determine the correct size
     const minDimension = Math.min(height, width);
 
+    const screenMargin = minDimension * (screenMarginPercentage / 100);
+
     if (Device.deviceType === Device.DeviceType.PHONE) {
-        // eslint-disable-next-line no-magic-numbers
-        if (minDimension <= 345) {
-            return 35;
-            // eslint-disable-next-line no-magic-numbers
-        } else if (minDimension <= 420) {
-            return 40;
-        } else if (minDimension <= 500) {
-            return 45;
-        }
-
-        return 60;
+        return Math.ceil((minDimension - screenMargin * 2) / 9);
     }
-
     if (minDimension <= 500) {
         return 20;
     } else if (minDimension <= 600) {
@@ -37,7 +28,9 @@ const getCellSize = (): number => {
 };
 
 export const CellSizeConstant = getCellSize();
+
 export const CellFontSizeConstant = CellSizeConstant / 2.5;
+
 export const CellCandidateVerticalOffsetConstant = CellSizeConstant * 0.05;
 export const CellCandidateHorizontalOffsetConstant = CellSizeConstant * 0.05;
 export const CellCandidateFontSizeConstant = CellSizeConstant / 3;
