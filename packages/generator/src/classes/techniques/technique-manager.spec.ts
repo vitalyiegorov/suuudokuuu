e6b2c29a089e62d551586bc3492e19f6ef0454df
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-14 07:46:05 +0100

    refactor(generator): remove unneeded methods

--- packages/generator/src/classes/techniques/technique-manager.spec.ts
+++ packages/generator/src/classes/techniques/technique-manager.spec.ts
@@ -3,13 +3,14 @@ import { describe, expect, it } from '@jest/globals';
 import { SolutionTechniqueEnum } from '../../enums/solution-technique.enum';
 import { defaultSudokuConfig } from '../../interfaces/sudoku-config.interface';
 import { SerializableSudoku } from '../serializable-sudoku/serializable-sudoku';
+import { Sudoku } from '../sudoku/sudoku';
 
 import { TechniqueManager } from './technique-manager';
 
 import type { FieldInterface } from '../../interfaces/field.interface';
 
 describe('TechniqueManager', () => {
-    const manager = new TechniqueManager();
+    const manager = new TechniqueManager(new Sudoku());
 
     const createFieldFromString = (fieldString: string): FieldInterface => {
         const [field] = SerializableSudoku.convertFieldFromString(fieldString, defaultSudokuConfig);
@@ -19,15 +20,7 @@ describe('TechniqueManager', () => {
 
     it('should identify FullHouse technique for last cell in unit', () => {
         const field = createFieldFromString(
-            '12345678.' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........'
+            '12345678.' + '.........' + '.........' + '.........' + '.........' + '.........' + '.........' + '.........' + '.........'
         );
 
         const cell = field[0][8];
@@ -38,15 +31,7 @@ describe('TechniqueManager', () => {
 
     it('should identify technique for a valid move', () => {
         const field = createFieldFromString(
-            '53..7....' +
-                '6..195...' +
-                '.98....6.' +
-                '8...6...3' +
-                '4..8.3..1' +
-                '7...2...6' +
-                '.6....28.' +
-                '...419..5' +
-                '....8..79'
+            '53..7....' + '6..195...' + '.98....6.' + '8...6...3' + '4..8.3..1' + '7...2...6' + '.6....28.' + '...419..5' + '....8..79'
         );
 
         const cell = field[0][2];
@@ -58,15 +43,7 @@ describe('TechniqueManager', () => {
 
     it('should identify HiddenSingle when value can only go in one place', () => {
         const field = createFieldFromString(
-            '.2345678.' +
-                '1........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........'
+            '.2345678.' + '1........' + '.........' + '.........' + '.........' + '.........' + '.........' + '.........' + '.........'
         );
 
         const cell = field[0][8];
@@ -78,15 +55,7 @@ describe('TechniqueManager', () => {
 
     it('should apply techniques in order of difficulty (easy to hard)', () => {
         const field = createFieldFromString(
-            '53..7....' +
-                '6..195...' +
-                '.98....6.' +
-                '8...6...3' +
-                '4..8.3..1' +
-                '7...2...6' +
-                '.6....28.' +
-                '...419..5' +
-                '....8..79'
+            '53..7....' + '6..195...' + '.98....6.' + '8...6...3' + '4..8.3..1' + '7...2...6' + '.6....28.' + '...419..5' + '....8..79'
         );
 
         const cell = field[0][2];
@@ -98,15 +67,7 @@ describe('TechniqueManager', () => {
 
     it('should fallback to Guess when no logical technique applies', () => {
         const field = createFieldFromString(
-            '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........' +
-                '.........'
+            '.........' + '.........' + '.........' + '.........' + '.........' + '.........' + '.........' + '.........' + '.........'
         );
 
         const cell = field[0][0];
