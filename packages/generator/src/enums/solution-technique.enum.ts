b0c1dfbf5abc4a77624d30ad890ab73851cd68a5
Vitalii Yehorov <vitalyiegorov@gmail.com>
2025-12-14 19:16:41 +0100

    feat(generator): locked candidate technique

--- packages/generator/src/enums/solution-technique.enum.ts
+++ packages/generator/src/enums/solution-technique.enum.ts
@@ -1,19 +1,7 @@
 export enum SolutionTechniqueEnum {
     Guess = 0,
-    FullHouse = 1,
     NakedSingle = 2,
     HiddenSingleGroup = 3,
     HiddenSingleLine = 4,
-    NakedPair = 5,
-    HiddenPair = 6,
-    NakedTriple = 7,
-    HiddenTriple = 8,
-    NakedQuad = 9,
-    HiddenQuad = 10,
-    PointingPair = 11,
-    XWing = 12,
-    Swordfish = 13,
-    Jellyfish = 14,
-    XYWing = 15,
-    XYZWing = 16
+    LockedCandidate = 5
 }
