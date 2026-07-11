f2934568b08da5830db11326752918fb82735417
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-07-11 15:16:35 +0200

    fix(app): resume game timer on route focus

--- packages/app/src/game/hooks/use-resume-game-on-focus.hook.ts
+++ packages/app/src/game/hooks/use-resume-game-on-focus.hook.ts
@@ -1,4 +1,4 @@
-import { type NativeStackNavigationProp, useNavigation } from 'expo-router';
+import { useNavigation } from 'expo-router';
 import { useEffect } from 'react';
 
 import { appRootStore } from '../../@generic/app-root.store';
@@ -6,23 +6,19 @@ import { useAppDispatch } from '../../@generic/hooks/use-app-dispatch.hook';
 import { gameResumeAction } from '../store/game.actions';
 import { gameIsStartedSelector, gamePausedSelector, gameShouldResumeOnFocusSelector } from '../store/game.selectors';
 
-type GameNavigationParamList = {
-    game: undefined;
-};
-
-export const useResumeGameAfterTransition = () => {
-    const navigation = useNavigation<NativeStackNavigationProp<GameNavigationParamList, 'game'>>();
+export const useResumeGameOnFocus = () => {
+    const navigation = useNavigation();
     const dispatch = useAppDispatch();
 
     useEffect(
         () =>
-            navigation.addListener('transitionEnd', ({ data }) => {
+            navigation.addListener('focus', () => {
                 const state = appRootStore.getState();
                 const hasStarted = gameIsStartedSelector(state);
                 const isPaused = gamePausedSelector(state);
                 const shouldResume = gameShouldResumeOnFocusSelector(state);
 
-                if (!data.closing && hasStarted && isPaused && shouldResume) {
+                if (hasStarted && isPaused && shouldResume) {
                     dispatch(gameResumeAction());
                 }
             }),
