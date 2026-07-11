3421ab4c7b76f56220c77f9e05747e3ffbfcd9f3
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-07-11 21:11:28 +0200

    fix(app): resume before settings removal

--- packages/app/src/game/hooks/use-resume-game-on-settings-close.hook.ts
+++ packages/app/src/game/hooks/use-resume-game-on-settings-close.hook.ts
@@ -1,4 +1,4 @@
-import { type NativeStackNavigationProp, useNavigation, useRoute } from 'expo-router';
+import { type NativeStackNavigationProp, useNavigation } from 'expo-router';
 import { useEffect } from 'react';
 
 import { appRootStore } from '../../@generic/app-root.store';
@@ -12,22 +12,20 @@ type GameSettingsNavigationParamList = {
 
 export const useResumeGameOnSettingsClose = () => {
     const navigation = useNavigation<NativeStackNavigationProp<GameSettingsNavigationParamList, 'game-settings'>>();
-    const route = useRoute();
     const dispatch = useAppDispatch();
 
     useEffect(
         () =>
-            navigation.addListener('transitionStart', ({ data, target }) => {
+            navigation.addListener('beforeRemove', () => {
                 const state = appRootStore.getState();
                 const hasStarted = gameIsStartedSelector(state);
                 const isPaused = gamePausedSelector(state);
                 const shouldResume = gameShouldResumeOnFocusSelector(state);
-                const didCloseGameSettings = data.closing && target === route.key;
 
-                if (didCloseGameSettings && hasStarted && isPaused && shouldResume) {
+                if (hasStarted && isPaused && shouldResume) {
                     dispatch(gameResumeAction());
                 }
             }),
-        [dispatch, navigation, route.key]
+        [dispatch, navigation]
     );
 };
