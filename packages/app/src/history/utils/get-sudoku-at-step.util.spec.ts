fb63fc5bfd08fd886e9bb422521b0491c462ddd5
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-07-12 07:14:39 +0200

    chore: update yarn and turbo build graph

--- packages/app/src/history/utils/get-sudoku-at-step.util.spec.ts
+++ packages/app/src/history/utils/get-sudoku-at-step.util.spec.ts
@@ -1,4 +1,3 @@
- 
 import { describe, expect, it } from '@jest/globals';
 import { Sudoku, defaultSudokuConfig } from '@suuudokuuu/generator';
 import { SolutionTechniqueEnum } from '@suuudokuuu/solver';
