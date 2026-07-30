7025b9062b9a80e259d62690c7eb7086bc54ff45
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-07-30 08:59:36 +0200

    feat(app): resolve custom themes in theme provider

--- packages/app/src/theme/components/theme-provider/theme-provider.tsx
+++ packages/app/src/theme/components/theme-provider/theme-provider.tsx
@@ -3,6 +3,8 @@ import { useEffect } from 'react';
 import { Appearance, Platform } from 'react-native';
 import { UnistylesRuntime } from 'react-native-unistyles';
 
+import { isDefined } from '@rnw-community/shared';
+
 import { useAppDispatch } from '../../../@generic/hooks/use-app-dispatch.hook';
 import { useAppSelector } from '../../../@generic/hooks/use-app-selector.hook';
 import { settingsSetAction } from '../../../settings/store/settings.actions';
@@ -10,6 +12,8 @@ import { settingsKeySelector, settingsThemeSelector } from '../../../settings/st
 import { ThemeContext } from '../../context/theme.context';
 import { ColorSchemaEnum } from '../../enum/color-schema.enum';
 import { customThemesSelector } from '../../store/custom-themes.selectors';
+import { isCustomThemeId } from '../../type-guard/is-custom-theme-id.type-guard';
+import { applyCustomUnistylesTheme } from '../../utils/apply-custom-unistyles-theme.util';
 import { getUnistylesThemeName } from '../../utils/get-unistyles-theme-name.util';
 import { resolveTheme } from '../../utils/resolve-theme.util';
 import { synchronizeUnistylesTheme } from '../../utils/synchronize-unistyles-theme.util';
@@ -24,15 +28,20 @@ interface Props {
 export const ThemeProvider = ({ children }: Props) => {
     const dispatch = useAppDispatch();
     const selectedTheme = useAppSelector(settingsThemeSelector);
-    const isDarkColorSchema = useAppSelector(settingsKeySelector('isDarkColorSchema'));
     const customThemes = useAppSelector(customThemesSelector);
+    const isDarkColorSchema = useAppSelector(settingsKeySelector('isDarkColorSchema'));
 
     const colorScheme = isDarkColorSchema ? ColorSchemaEnum.Dark : ColorSchemaEnum.Light;
     const unistylesThemeName = getUnistylesThemeName(selectedTheme, colorScheme);
+    const activeCustomTheme = customThemes.find(theme => isCustomThemeId(selectedTheme) && theme.id === selectedTheme);
 
     useEffect(() => {
+        if (isDefined(activeCustomTheme)) {
+            applyCustomUnistylesTheme(UnistylesRuntime, activeCustomTheme);
+        }
+
         synchronizeUnistylesTheme(UnistylesRuntime, unistylesThemeName);
-    }, [unistylesThemeName]);
+    }, [activeCustomTheme, unistylesThemeName]);
 
     const changeTheme = (theme: SettingsState['theme']) => {
         dispatch(settingsSetAction({ theme }));
