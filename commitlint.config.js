ee3ecc49e481e0286aae9ca943501567942229b7
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-07-30 13:49:56 +0200

    chore: allow the documented i18n commit type in commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional', '@commitlint/config-workspace-scopes'],
+    rules: {
+        'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'i18n', 'perf', 'refactor', 'revert', 'style', 'test']]
+    },
     ignores: [message => message.includes('WIP')]
 };
