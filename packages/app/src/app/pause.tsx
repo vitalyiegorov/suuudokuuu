019856bbbd39380d3b7834ad0bac595ca51b6cc6
Vitalii Yehorov <vitalyiegorov@gmail.com>
2026-08-11 06:52:17 +0200

    fix(app): redirect cold game and pause route entries to home

--- packages/app/src/app/pause.tsx
+++ packages/app/src/app/pause.tsx
@@ -1,11 +1,19 @@
 import { useLingui } from '@lingui/react/macro';
+import { Redirect } from 'expo-router';
 
 import { Page } from '../@generic/components/page/page';
 import { PageHeader } from '../@generic/components/page-header/page-header';
+import { useAppSelector } from '../@generic/hooks/use-app-selector.hook';
+import { gameIsStartedSelector } from '../game/store/game.selectors';
 import { PauseScreen } from '../screens/components/pause-screen/pause.screen';
 
 export default function PausePage() {
     const { t } = useLingui();
+    const isGameStarted = useAppSelector(gameIsStartedSelector);
+
+    if (!isGameStarted) {
+        return <Redirect href="/" />;
+    }
 
     return (
         <Page>
